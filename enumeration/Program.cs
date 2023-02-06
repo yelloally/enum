432c404ef7133c3332386b85bf612655c3187050
Alona <122108963+yelloally@users.noreply.github.com>
2023-02-06 12:09:16 +0100

    Add files via upload

---
+++ enumeration/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+    enum year
+    {
+
+        // items of the enum
+        January,
+        February,
+        March,
+        April,
+        May,
+        June
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Printing out the integer values of the items
+            Console.WriteLine("The value of January in year " + "enum is " + (int)year.January);
+            Console.WriteLine("The value of February in year " + "enum is " + (int)year.February);
+            Console.WriteLine("The value of March in year " + "enum is " + (int)year.March);
+            Console.WriteLine("The value of April in year " + "enum is " + (int)year.April);
+            Console.WriteLine("The value of May in year " + "enum is " + (int)year.May);
+            Console.WriteLine("The value of June in year " + "enum is " + (int)year.June);
+        }
+    }
